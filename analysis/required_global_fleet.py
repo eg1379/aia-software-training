a2e4295db7a1cd17113d1902c25a157f207ee0f0
Ewan Gribbin <eg587@cam.ac.uk>
2025-09-18 12:34:14 +0100

    Format Python code using `ruff format`

--- analysis/required_global_fleet.py
+++ analysis/required_global_fleet.py
@@ -6,5 +6,7 @@ seats_per_aircraft = 150.0
 flights_per_aircraft_per_day = 2.0
 
 passengers_per_day = aviation.passengers_per_day(passengers_per_year, days_per_year)
-required_global_fleet = aviation.required_global_fleet(passengers_per_day, seats_per_aircraft, flights_per_aircraft_per_day)
+required_global_fleet = aviation.required_global_fleet(
+    passengers_per_day, seats_per_aircraft, flights_per_aircraft_per_day
+)
 print(f"{required_global_fleet=}")
