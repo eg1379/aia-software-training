c86fa87756b4a205fb7fbc90d0a218a092c36bed
Ewan Gribbin <eg587@cam.ac.uk>
2025-09-19 15:36:27 +0100

    Add missing docstrings to scripts, modules, and functions

--- src/aviation/aviation.py
+++ src/aviation/aviation.py
@@ -1,6 +1,11 @@
+"""Modelling of the global fleet based on average passenger and aircraft data."""
+
+
 def passengers_per_day(passengers_per_year, days_per_year):
+    """The number of passengers per day globally."""
     return passengers_per_year / days_per_year
 
 
 def required_global_fleet(passengers_per_day, seats_per_aircraft, flights_per_aircraft_per_day):
+    """The size of the required global fleet."""
     return passengers_per_day / (seats_per_aircraft * flights_per_aircraft_per_day)
