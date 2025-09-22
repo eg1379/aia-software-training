a224ab34aa84b60bdce61ddedd885e9dfcbc6f73
Ewan Gribbin <eg587@cam.ac.uk>
2025-09-22 13:46:01 +0100

    Remove argument types from docstrings

--- src/aviation/fleet.py
+++ src/aviation/fleet.py
@@ -5,8 +5,8 @@ def passengers_per_day(passengers_per_year: float, days_per_year: float) -> floa
     """The number of passengers per day globally.
 
     Args:
-        passengers_per_year (float): The number of passengers flying per year globally.
-        days_per_year (float): The number of days in the modelled year.
+        passengers_per_year: The number of passengers flying per year globally.
+        days_per_year: The number of days in the modelled year.
     """
     return passengers_per_year / days_per_year
 
@@ -17,9 +17,9 @@ def required_global_fleet(
     """The size of the required global fleet.
 
     Args:
-        passengers_per_day (float): The number of passengers flying per day globally.
-        seats_per_aircraft (float): The average number of seats on a commercial aircraft.
-        flights_per_aircraft_per_day (float): The average number of flights a commercial aircraft makes on
+        passengers_per_day: The number of passengers flying per day globally.
+        seats_per_aircraft: The average number of seats on a commercial aircraft.
+        flights_per_aircraft_per_day: The average number of flights a commercial aircraft makes on
             average per day.
     """
     return passengers_per_day / (seats_per_aircraft * flights_per_aircraft_per_day)
