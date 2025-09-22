d4f35386bb3cf66305a40ff3205f50766f5c96c4
Ewan Gribbin <eg587@cam.ac.uk>
2025-09-22 14:42:03 +0100

    Refactor analysis scripts to use `SystemsModel.evaluate`

--- analysis/required_global_fleet.py
+++ analysis/required_global_fleet.py
@@ -1,14 +1,21 @@
 """Analysis to determine the required size of the global fleet."""
 
 import aviation
+from aviation import _engine as engine
 
-days_per_year = 365.0
 passengers_per_year = 5_000_000_000.0
-seats_per_aircraft = 150.0
-flights_per_aircraft_per_day = 2.0
+days_per_year = 365.0
+seats_per_aircraft = 200.0
+flights_per_aircraft_per_day = 3.0
 
-passengers_per_day = aviation.passengers_per_day(passengers_per_year, days_per_year)
-required_global_fleet = aviation.required_global_fleet(
-    passengers_per_day, seats_per_aircraft, flights_per_aircraft_per_day
-)
+inputs = {
+    "passengers_per_year": passengers_per_year,
+    "days_per_year": days_per_year,
+    "seats_per_aircraft": seats_per_aircraft,
+    "flights_per_aircraft_per_day": flights_per_aircraft_per_day,
+}
+output = "required_global_fleet"
+
+systems_model = engine.SystemsModel(aviation.transforms)
+required_global_fleet = systems_model.evaluate(inputs, output)
 print(f"{required_global_fleet=}")
